50ee29c9f743f0600be22e271cd4b6099ac69d07
mwj1270131411 <32675571+mwj1270131411@users.noreply.github.com>
2018-06-13 15:43:03 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import Root from './src/Root';
+
+AppRegistry.registerComponent('xyxcdjk', () => Root);
---
+++ src/container/App.js
@@ -0,0 +1,17 @@
+//整个app的入口
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+import { StackNavigator } from 'react-navigation';
+import LoginPage from '../pages/LoginPage'
+import MainPage from '../pages/MainPage'
+
+const App = StackNavigator({
+    Login: { screen: LoginPage },
+    Main: { screen: MainPage},
+});
+
+export default App
